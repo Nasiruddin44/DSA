281912f5e6c4744852c31f1f724fc597f4bb355b
Nasiruddin44 <khannasiruddin44@gmail.com>
2024-02-10 11:37:50 +0530

    main

---
+++ Heaps/PriorityQueues.java
@@ -0,0 +1,52 @@
+
+/* 
+import java.util.*;
+
+public class PriorityQueues {
+    public static void main(String[] args) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
+        pq.add(3);
+        pq.add(4);
+        pq.add(1);
+        pq.add(7);
+        while (!pq.isEmpty()) {
+            System.out.println(pq.peek());
+            pq.remove();
+        }
+    }
+
+}
+*/
+import java.util.*;
+
+public class PriorityQueues {
+
+    static class Student implements Comparable<Student> {
+        String name;
+        int rank;
+
+        public Student(String name, int rank) {
+            this.name = name;
+            this.rank = rank;
+        }
+
+        @Override
+        public int compareTo(Student s2) {
+            return this.rank - s2.rank;
+        }
+    }
+
+    public static void main(String[] args) {
+        PriorityQueue<Student> pq = new PriorityQueue<>(Comparator.reverseOrder());
+        pq.add(new Student("A", 4));
+        pq.add(new Student("B", 5));
+        pq.add(new Student("C", 2));
+        pq.add(new Student("D", 12));
+
+        while (!pq.isEmpty()) {
+            System.out.println(pq.peek().name + "->" + pq.peek().rank);
+            pq.remove();
+        }
+
+    }
+}
